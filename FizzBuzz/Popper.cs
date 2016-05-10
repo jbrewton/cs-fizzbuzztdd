d5354b7b6629a4de4b18a6848351b16cbd457d4b
Joshua Brewton <jebrewton@gmail.com>
2016-05-10 11:17:47 -0400

    tdd the fizz buzz

---
+++ FizzBuzz/Popper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Popper
+    {
+        public string apply(int item)
+        {
+            string result;
+            StringBuilder sb = new StringBuilder();
+
+            fizz(item, ref sb);
+            buzz(item, ref sb);
+
+            if (sb.Length == 0) {
+                result = item.ToString();
+            }
+            else {
+                result = sb.ToString();
+            }
+            return result;
+        }
+
+        private void fizz(int item, ref StringBuilder s)
+        {
+            if(item % 3 == 0)
+            {
+                s.Append("fizz");
+            }
+        }
+
+        private void buzz(int item, ref StringBuilder s)
+        {
+            if (item % 5 == 0)
+            {
+                s.Append("buzz");
+            }
+        }
+
+    }
+}
---
+++ FizzBuzzTest/PopperTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FizzBuzz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTest
+{
+    [TestClass]
+    public class PopperTest
+    {
+        [TestMethod]
+        public void TestDefault()
+        {
+            int item = 1;
+            Popper p = new Popper();
+            string actual = p.apply(item);
+            Assert.AreEqual("1", actual);
+        }
+        [TestMethod]
+        public void TestFizz()
+        {
+            int item = 3;
+            Popper p = new Popper();
+            string actual = p.apply(item);
+            Assert.AreEqual("fizz", actual);
+        }
+        [TestMethod]
+        public void TestBuzz()
+        {
+            int item = 5;
+            Popper p = new Popper();
+            string actual = p.apply(item);
+            Assert.AreEqual("buzz", actual);
+        }
+        [TestMethod]
+        public void TestFizzBuzz()
+        {
+            int item = 15;
+            Popper p = new Popper();
+            string actual = p.apply(item);
+            Assert.AreEqual("fizzbuzz", actual);
+        }
+
+    }
+}
